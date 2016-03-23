6854aff0bf2154a71f6353ec8c22a908a1270779
se15m015 <se15m015@technikum-wien.at>
2016-03-23 13:24:08 +0100

    QuickSort_Serial.cs done

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,27 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
-            //int[] numbers = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
+            Stopwatch sw = new Stopwatch();
+            //int[] numbers = {3,1,4,1,345,509,90,3,4,675,2,12,6,5,3};
+            sw.Start();
             var numbers = Helper.RandomNumbers(1000);
-            QuickSort.QuickSort_Recursive(numbers, 0, numbers.Length - 1);
+            sw.Stop();
+            Console.WriteLine("Randomizer done lenght: {0}, time taken: {1}", numbers.Length, sw.ElapsedMilliseconds);
+            sw.Restart();
+            QuickSort_Serial.QuickSort_Recursive(numbers, 0, numbers.Length - 1);
 
+            List<int> checkList = new List<int>();
             for (int i = 0; i < numbers.Length; i++)
             {
                 Console.WriteLine(numbers[i]);
+                if (checkList.Any(x => i < x))
+                {
+                    Console.WriteLine("Booooooeese");
+                }
+                checkList.Add(i);
             }
-
-            Console.WriteLine();
+            sw.Stop();
+            Console.WriteLine("Sorting time taken: {0}", sw.ElapsedMilliseconds);
             Console.ReadLine();
         }
     }
---
+++ Sorting/QuickSort_Serial.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//reference:
+//http://me.dt.in.th/page/Quicksort/
+
+namespace Sorting
+{
+    public class QuickSort_Serial
+    {
+        public static int Partition(int[] numbers, int first, int last)
+        {
+            int pivot = numbers[first];
+            int indexFirstOpen = first+1;
+            int indexLastClosed = first;
+
+            for (int i = first+1; i <= last; i++)
+            {
+                if(numbers[i] < pivot)
+                {
+                    Swap(numbers, indexFirstOpen, i);
+                    indexLastClosed = indexFirstOpen;
+                    indexFirstOpen++;
+                }
+            }
+
+            Swap(numbers, first, indexLastClosed);
+
+            return indexLastClosed;
+        }
+
+        private static void Swap(int[] numbers, int indexFirstOpen, int i)
+        {
+            int tmp = numbers[indexFirstOpen];
+            numbers[indexFirstOpen] = numbers[i];
+            numbers[i] = tmp;
+        }
+
+        public static void QuickSort_Recursive(int[] numbers, int left, int right)
+        {
+            if ((right - left)  < 1)
+            {
+                return;
+            }
+
+            int pivot = Partition(numbers, left, right);
+            QuickSort_Recursive(numbers, left, pivot - 1);
+            QuickSort_Recursive(numbers, pivot+1, right);
+        }
+    }
+}
