94b139da067d231d2aec5334bec0492514b0f16c
se15m015 <se15m015@technikum-wien.at>
2016-03-23 10:24:54 +0100

    wip 1

---
+++ Sorting/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class Helper
+    {
+        public static int[] RandomNumbers(int length)
+        {
+            int[] rng = new int[length];
+            Random rand = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < length; i++)
+            {
+                rng[i] = rand.Next(0,999999);
+            }
+            return rng;
+        }
+    }
+}
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -10,6 +10,17 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
+            //int[] numbers = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
+            var numbers = Helper.RandomNumbers(1000);
+            QuickSort.QuickSort_Recursive(numbers, 0, numbers.Length - 1);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
